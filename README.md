1d0cc97d780dcc6adb391dd71778c3c433674f36
Taylor <taysully94@gmail.com>
2023-08-31 20:44:30 -0400

    Formats correctly, adds notes, adds README content

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# day-planner
-## to help plan your day! 
+# Day-Planner
 
+## To help plan your work day!
+
+This day planner was created to help you plan your tasks for the day as meticulously as you want.
+Your schedule will never get out of hand with this amazing new tool. It will even remember your past entries! Cool!
+
+### Usage
+
+Please visit my [Site!](https://tayskully.github.io/day-planner/)
+In order to get the full experience, users must enter in their work-day activities into the text boxes, and click the save button on the right. If the user's current time is within working hours-- the past hours will be grey, the present hour will be red, and the future hours will be green. Once you enter your schedule and hit save, the data will be waiting for you when you get back.
+
+The interaction may look like the following screenshots:
+
+![photo of the work day planner](./assets/images/screengrab1.png)
+![the workd day planner in action](./assets/images/screengrab2.png)
+
+### Credits
+
+The collaborators on this project include me, myself, and Taylor
+@tayskully
+third-party assets
+Tutorials followed include Columbia University EdX's Coding Bootcamp 2023, as well as:  
+ [Github Docs](https://docs.github.com/en)  
+ [developer mozilla](https://developer.mozilla.org/en-US/)  
+ [Dev.to](https://dev.to/)  
+ [Code Academy blogs](https://www.codecademy.com/resources/blog/)
+
+Special thanks to my tutor Erik Hirsch for helping me debug!
+
+### License
+
+MIT License
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(document).ready();
-//DEPENDENCIES ===========
-
-// var calendarInput = $("textarea");
-var calendarInputDate = $("#hour"); //how to target the changing element?? add new id??
-// DATA ================
-
 //FUNCTIONS====================
+$(document).ready();
+
+// displays the current date in the header of the page
 var today = dayjs();
 var dayWeek = today.format("dddd MMM D, YYYY");
 $("#currentDay").text(dayWeek);
-
+//generates time blocks 
 $(function () {
   var timeBlocksContainer = $("#time-blocks");
   var timeBlocks = generateTimeBlocks();
-  console.log(timeBlocks);
-
   function generateTimeBlocks() {
     var startingHour = 8;
     var endingHour = 17;
@@ -36,13 +27,13 @@ $(function () {
   function renderTimeBlocks(timeBlocks) {
     var thisHour = parseInt(dayjs().format("H"));
     var pastPresentFuture;
-
+    //adds am  / pm to rendered timeblocks
     timeBlocksContainer.empty(); //jquery
     for (var i = 0; i < timeBlocks.length; i++) {
       var amPm = timeBlocks[i].time;
       if (amPm >= 12) amPm = "pm";
       else amPm = "am";
-
+      // Adds code to apply the past, present, or future class to each time block
       if (timeBlocks[i].time < thisHour) {
         pastPresentFuture = "past";
       } else if (timeBlocks[i].time === thisHour) {
@@ -50,7 +41,9 @@ $(function () {
       } else {
         pastPresentFuture = "future";
       }
-
+      //converts military time to am/pm time
+      //gets entered data from local storage (if any)
+      //renders timeblock content
       var hour = timeBlocks[i].time % 12;
       if (hour === 0) hour = 12;
       var saveNote = localStorage.getItem(hour + amPm) || "";
@@ -65,35 +58,16 @@ $(function () {
       timeBlocksContainer.append(newTimeBlockEl);
     }
   }
+  // adds data to local storage
   renderTimeBlocks(timeBlocks);
   var saveBtn = $(".saveBtn");
+  // Adds a listener for click events on the save button
   saveBtn.on("click", function (event) {
     event.preventDefault();
     var siblings = $(this).siblings();
-
     var time = siblings[0].textContent;
-    
     var calInput = siblings[1].value;
-
     localStorage.setItem(time, calInput);
   });
 });
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
